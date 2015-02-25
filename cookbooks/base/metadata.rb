492029b8624341066e1f7de4dd4d82e2f69e61f0
Teimatini Marin <teimatinim@trusty64>
2015-02-25 23:20:30 +0000

    Installs OpenJava & and closes #7

--- cookbooks/base/metadata.rb
+++ cookbooks/base/metadata.rb
@@ -4,5 +4,8 @@ maintainer_email 'tmarin@beuwa.com'
 license          'all_rights'
 description      'Installs/Configures shelf'
 long_description 'Installs/Configures shelf'
-version          '0.1.0'
+version          '0.2.0'
+
+depends          'apt',  '~> 2.6.1'
+depends          'java', '~> 1.31.0'
 
